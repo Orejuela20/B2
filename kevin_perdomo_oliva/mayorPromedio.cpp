377423ac6420c9c303ca3fb9a5ad78738561767b
GerarGarces <janeth.altafuya.mideros@utelvt.edu.ec>
2022-10-24 22:49:18 +0000

    programa de moyorPromedio.cpp

---
+++ kevin_perdomo_oliva/mayorPromedio.cpp
@@ -0,0 +1,28 @@
+//programa para saber el mayor promedio
+//nombre:kevin perdomo oliva
+//asignatura:Tics B
+//docente:Ing.Stalin Francis
+
+#include<iostream>
+using namespace std;
+int main()
+
+{
+	float x,pm=0;
+	int i=0,l;
+	cout<<"ingrese l: "; cin>>l;
+	do{
+
+	cout<<"ingrese x:  "; cin>>x;
+	i=i+1;
+	if(x>pm){
+
+		pm=x;
+
+	}
+ 
+	}while(i<l);
+	cout<<"el promedio maximo del curso fue:  "<<pm<<endl<<endl;
+	return 0;
+
+}
