0fa4ad8a8694bc6039af5c4a2c6d1c438fb8d446
GerarGarces <janeth.altafuya.mideros@utelvt.edu.ec>
2022-10-24 22:26:51 +0000

    programa de calculaedad.cpp

---
+++ kevin_perdomo_oliva/calculaEdad.cpp
@@ -0,0 +1,37 @@
+//programa creado para calcular edad
+//nombre:kevin perdomo oliva
+//asignatura:Tics B
+//docente:Ing.Stalin Francis
+
+#include<iostream>
+using namespace std;
+int main()
+
+{
+	int aa,ma,da,an,mn,dn,a,m,d;
+	cout<<"ingrese la fecha actual: ";
+	cin>>aa>>ma>>da;
+
+	cout<<"ingrese la fecha de nacimiento:  ";
+	cin>>an>>mn>>dn;
+	if(da>dn){
+		d=da-dn;
+
+	}else{
+		da=da+30;
+		ma=ma-1;
+		d=da-dn;
+
+	}
+	if(ma>mn){
+		m=ma-mn;
+	}else{
+		ma=ma+12;
+		aa=aa-an;
+		m=ma-mn;
+	}
+	a=aa-an;
+	cout<<"usted tiene:  "<<a<<"años,"<<d<<"meses,"<<d<<"dias,"<<endl;
+	return 0;
+
+}
