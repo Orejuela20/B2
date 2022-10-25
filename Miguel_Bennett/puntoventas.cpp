eaf5d31d94c74945d03ebb85ed9f14d7fcf53ab4
El Migue <miguel.bennett.valencia@utelvt.edu.ec>
2022-10-25 10:21:14 -0500

    punto de ventas

---
+++ Miguel_Bennett/puntoventas.cpp
@@ -0,0 +1,29 @@
+//Programa: Punto de Ventas  
+ //Nombre:Miguel Bennett Valencia  
+ //Asignatura:Tics B 
+ //Docente:Ing.Stalin Francis 
+   
+ #include<iostream> 
+ using namespace std; 
+ int main() 
+  
+ {       
+  
+         float x,s=0,vb,piva=12,viva,pdesc=10,vdesc,vn; 
+         int i=0,l; 
+         cout<<"ingrese l:  ";cin>>l; 
+         do{ 
+          
+         cout<<"ingrese x:  ";cin>>x; 
+         i=i+1; 
+         s=s+x; 
+          
+         }while(i<l); 
+         vb=s; 
+         viva=vb*piva/100; 
+         vdesc=vb*pdesc/100; 
+         vn=vb+viva-vdesc; 
+         cout<<"el valor a pagar es de:  "<<vn<<endl<<endl; 
+         return 0; 
+  
+ }
