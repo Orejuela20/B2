3a966dbb125c6572f8220b53464c722e420209d3
El Migue <miguel.bennett.valencia@utelvt.edu.ec>
2022-10-26 07:14:03 -0500

    saldofinal

---
+++ Miguel_Bennett/saldofinal.cpp
@@ -0,0 +1,28 @@
+//Programa creado para conocer el saldo final 
+ //Nombre:Miguel Bennett Valencia  
+ //Asignatura: Tics B 
+ //Docemte: Ing.Stalin Francis 
+  
+ #include<iostream> 
+ using namespace std; 
+ int main() 
+  
+ { 
+         float x,s=0; 
+         int i=0,l; 
+         cout<<"ingrese cantidad de egreso (l):  "; 
+                 cin>>l; 
+         cout<<"ingrese saldo inicial (s):  "; 
+         cin>>s; 
+  
+         do{ 
+          
+         cout<<"ingrese egreso (x):  "; 
+         cin>>x; 
+         i=i+1; 
+         s=s+x; 
+         }while(i<l); 
+         cout<<"el saldo final es:  "<<s<<endl; 
+         return 0; 
+  
+ }
